0a02e5b09c90cd28ca45c43678fbe383d412302f
nipsii <urra271206@gmail.com>
2026-03-18 15:25:47 +0100

    implemented street lights and real time money check

--- world.gd
+++ world.gd
@@ -4,8 +4,12 @@ extends Node2D
 @onready var api = $APIClient
 
 func _ready() -> void:
+	api.money_updated.connect(_on_money_updated)
+	
 	var amount = api.get_money()
-	
 	money_label.text = str(amount)
-	
-	print("UI Updated with balance: ", amount)
+	print("UI Initialized with balance: ", amount)
+
+func _on_money_updated(new_amount: int) -> void:
+	money_label.text = str(new_amount)
+	print("UI Auto-Updated to: ", new_amount)
